a92fcbb7ecd2f0b82e07f42f55a8ab34a5e18afe
Tibebu-ES <tibebuenyew@gmail.com>
2021-09-29 15:17:37 +0300

    change base url, for the purpose of deployment

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://rauno-test.herokuapp.com/';
+    public $baseURL = 'http://rauno-test.herokuapp.com/public';
 
     /**
      * --------------------------------------------------------------------------
